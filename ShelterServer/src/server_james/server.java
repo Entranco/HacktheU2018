2a6a0c84746b76a6121af61dcca6870689715119
Streetpope <43098353+Streetpope@users.noreply.github.com>
2018-09-08 23:08:27 -0600

    Update server.java

--- ShelterServer/src/server_james/server.java
+++ ShelterServer/src/server_james/server.java
@@ -1,8 +1,10 @@
-package server_james;
+package hacktheU;
+
 
 
 
 import java.net.*;
+import java.util.Scanner;
 import java.io.*;
 
 //import 	android.graphics.Bitmap;
@@ -19,7 +21,61 @@ public class server extends Thread {
       serverSocket = new ServerSocket(8888);
       //serverSocket.setSoTimeout(10000);
    }
-
+   
+   public String requestType(String request) {
+	   // break up string - if statement to decide next path
+	   Scanner scnr = new Scanner(request);
+	   String requestType;
+	   requestType = scnr.next();
+	   String s1 = scnr.next();
+	   String s2 = scnr.next();
+	   //for request to see a new pet
+	   if(requestType.equals("nextPet")) {
+		   
+		   return handleRequestInfo(s1,s2) + " " + handleRequestImage(s1,s2);
+		   
+	   }
+	   if(requestType.equals("login")) {
+		   
+		   return loginHandler(s1,s2);
+	   }
+	   if(requestType.equals("contact")) {
+		   
+		   return contact(s1,s2);
+	   }
+	   return"";
+   }
+   public String handleRequestInfo(String filter,String queryNumber) {
+	   //read from database
+	   
+	   
+	   return filter + queryNumber + "ran through handleRequest ";
+   }
+   public String loginHandler(String userName,String password) {
+	   //read from database
+	   
+	   
+	   return userName + password + "ran through loginHandler ";
+   }
+   public String handleRequestImage(String filter,String queryNumber) {
+	   //read from database
+	   
+	   
+	   return filter + queryNumber + "ran through requestImage ";
+   }
+   public String contact(String filter,String queryNumber) {
+	   //read from database
+	   
+	   
+	   return filter + queryNumber + "ran through contact ";
+   }
+   public String shelterReqeust(String filter,String queryNumber) {
+	   //read from database
+	   
+	   
+	   return filter + queryNumber + "ran through shelterRequest ";
+   }
+   
    public void run() {
       while(true) {
          try {
@@ -32,9 +88,9 @@ public class server extends Thread {
            
             OutputStream outStream = server.getOutputStream();
             DataOutputStream dataOut = new DataOutputStream(outStream);
-           System.out.println(in.readUTF());
            
-           dataOut.writeUTF("sent from server");
+           
+           dataOut.writeUTF(requestType(in.readUTF()));
            
           // DataOutputStream out = new DataOutputStream(server.getOutputStream());
          
