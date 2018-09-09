0d78998361e54283041206646d56ba8cd714ea07
Entranco <39010159+Entranco@users.noreply.github.com>
2018-09-09 01:52:08 -0600

    Revert "Brings back ShelterApp"
    This reverts commit 1a7486e4f1d356f0adb2957df8533eb8b128539a.

--- ShelterServer/src/server/Database.java
+++ ShelterServer/src/server/Database.java
@@ -5,13 +5,5 @@ import java.util.ArrayList;
 public class Database {
 	public ArrayList<ShelterAnimal> animals;
 	
-	public Database() {
-		fileToDatabase();
-		
-	}
-
-	private void fileToDatabase() {
-		
-	}
 	
 }
